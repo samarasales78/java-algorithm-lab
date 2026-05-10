b11c904f3b0733014f2883dc6644c483f11bb00f
samarasales78 <smatiassales@gmail.com>
2026-05-10 19:39:59 -0300

    add arithmetic mean exercise

--- ex1.java
+++ ex1.java
@@ -1,3 +1,25 @@
-public class ex1 {
-  
+import java.util.Scanner;
+import java.util.Locale;
+
+public class Ex1 {
+
+  public static void main(String[] args) {
+
+    Scanner s = new Scanner(System.in);
+    s.useLocale(Locale.US);
+
+    System.out.print("Digite o primeiro número: ");
+    double n1 = s.nextDouble();
+
+    System.out.print("Digite o segundo número: ");
+    double n2 = s.nextDouble();
+
+    System.out.print("Digite o terceiro número: ");
+    double n3 = s.nextDouble();
+    
+    double media = (n1 + n2 + n3) / 3;
+    System.out.printf("Média: %.2f", media);
+
+    s.close();
+  }
 }
