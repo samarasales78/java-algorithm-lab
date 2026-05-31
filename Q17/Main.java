0bbe84101902a73b2b39c5255f3a9b558a497d30
samarasales78 <smatiassales@gmail.com>
2026-05-31 08:15:20 -0300

    add sum project

---
+++ Q17/Main.java
@@ -0,0 +1,13 @@
+package Q17;
+
+public class Main {
+  
+  public static void main(String[] args) {
+
+    int[] numerosArray = {10, 20, 30, 40};
+
+    Numeros n1 = new Numeros(numerosArray);
+
+    System.out.println("Soma = " + n1.calcularSoma());
+  }
+}
---
+++ Q17/Numeros.java
@@ -0,0 +1,20 @@
+package Q17;
+
+public class Numeros {
+  
+  private int[] valores;
+
+  public Numeros(int[] valores) {
+    this.valores = valores;
+  }
+
+  public int calcularSoma() {
+    int soma = 0;
+
+    for (int i = 0; i < valores.length; i++) {
+      soma += valores[i];
+    }
+
+    return soma;
+  }
+}
