ef72ce5d2cee91b5f3de0bf993d858d2f69d162e
samarasales78 <smatiassales@gmail.com>
2026-05-31 08:24:12 -0300

    implement max value search in array

---
+++ Q18/Main.java
@@ -0,0 +1,13 @@
+package Q18;
+
+public class Main {
+
+  public static void main(String[] args) {
+
+    int[] valores = {15, 8, 42, 27, 3};
+
+    Vetor v1 = new Vetor(valores);
+
+    System.out.println("Maior valor = " + v1.maiorValor());
+  }
+}
---
+++ Q18/Vetor.java
@@ -0,0 +1,24 @@
+package Q18;
+
+public class Vetor {
+  
+  private int[] numeros;
+
+  public Vetor(int[] numeros) {
+    this.numeros = numeros;
+  }
+
+  public int maiorValor() {
+
+    int maior = numeros [0]; // assume que o primeiro é o maior
+
+    for (int i = 1; i < numeros.length; i++) { // percorre array
+
+      if (numeros[i] > maior) { // compara
+        maior = numeros[i];
+      }
+    }
+
+    return maior;
+  }
+}
