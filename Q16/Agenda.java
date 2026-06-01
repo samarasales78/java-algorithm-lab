6b435a621e57b8e47708c62064a772cd4c6c3947
samarasales78 <smatiassales@gmail.com>
2026-06-01 16:54:54 -0300

    contact system

--- Q16/Agenda.java
+++ Q16/Agenda.java
@@ -4,44 +4,32 @@ import java.util.ArrayList;
 
 public class Agenda {
 
-    // ArrayList de contatos
-    private ArrayList<Contato> contatos;
+  private ArrayList<Contato> contatos;
 
-    // construtor
-    public Agenda() {
-        contatos = new ArrayList<>();
+  public Agenda() {
+    contatos = new ArrayList<>();
+  }
+
+  public void adicionarContato(Contato contato) {
+    contatos.add(contato);
+  }
+
+  public void listarContatos() {
+    for (Contato c : contatos) {
+      c.exibirContato();
+    }
+  }
+
+  public void pesquisarContato(String nome) {
+    
+    for (Contato c : contatos) {
+
+      if (c.getNome().equalsIgnoreCase(nome)) {
+        c.exibirContato();
+        return;
+      }
     }
 
-    // adicionar contato
-    public void adicionarContato(Contato contato) {
-        contatos.add(contato);
-    }
-
-    // listar contatos
-    public void listarContatos() {
-
-        System.out.println("LISTA DE CONTATOS");
-
-        for (Contato c : contatos) {
-            c.exibirContato();
-        }
-    }
-
-    // pesquisar contato pelo nome
-    public void pesquisarContato(String nome) {
-
-        boolean encontrado = false;
-
-        for (Contato c : contatos) {
-
-            if (c.getNome().equalsIgnoreCase(nome)) {
-                c.exibirContato();
-                encontrado = true;
-            }
-        }
-
-        if (!encontrado) {
-            System.out.println("Contato não encontrado.");
-        }
-    }
+      System.out.println("Contato não encontrado.");
+  }
 }
--- Q16/Contato.java
+++ Q16/Contato.java
@@ -2,27 +2,24 @@ package Q16;
 
 public class Contato {
 
-  // atributos privados
   private String nome;
   private String telefone;
 
-  // construtor
   public Contato(String nome, String telefone) {
-      this.nome = nome;
-      this.telefone = telefone;
+    this.nome = nome;
+    this.telefone = telefone;
   }
 
-  // getters
   public String getNome() {
-      return nome;
+    return nome;
   }
 
   public String getTelefone() {
-      return telefone;
+    return telefone;
   }
 
-  // exibir contato
   public void exibirContato() {
-      System.out.println("Nome: " + nome + " | Telefone: " + telefone);
+    System.out.printf("Nome: " + nome, "Telefone: " + telefone);
   }
+  
 }
